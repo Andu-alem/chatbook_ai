dcb01921f6ce0b82ecc373c72f08f67853f5c6e6
Andualem <andualem.fereja12@gmail.com>
2025-08-23 14:06:43 +0300

    chore(backend): create settings config

---
+++ backend/config.py
@@ -0,0 +1,13 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Settings:
+    SECRET_KEY: str = os.getenv("SECRET_KEY")
+    MONGODB_URI: str = os.getenv("MONGODB_URI")
+    DB_NAME: str = os.getenv("DB_NAME")
+    GOOGLE_API_KEY: str = os.getenv("GOOGLE_API_KEY")
+
+
+settings = Settings()
