8e6bdfbf7c64476fad4bf59d9354afb4d3f8265b
Andualem <andualem.fereja12@gmail.com>
2025-08-23 20:13:28 +0300

    chore(backend): create books meta data

---
+++ backend/index_handler/books_meta.py
@@ -0,0 +1,66 @@
+books_data = []
+books_data.append({
+    "title": "The Art Of Laziness",
+    "author": "Library Mindset",
+    "genere": "Personal Development, Self-Help, Productivity",
+    "file_name": "the_art_of_laziness.pdf",
+    "relevant_start_page": 7,
+    "relevant_last_page": -4
+})
+
+books_data.append({
+    "title": "The Alchemist",
+    "author": "Paulo Coelho",
+    "genere": "Inspirational",
+    "file_name": "the_alchemist.pdf",
+    "relevant_start_page": 9,
+    "relevant_last_page": -9
+})
+books_data.append({
+    "title": "As a Man Thinkth",
+    "author": "James Allen",
+    "genere": "Self-Help, Philosopy, Personal Development",
+    "file_name": "as_a_man_thinth.pdf",
+    "relevant_start_page": 7,
+    "relevant_last_page": -4
+})
+books_data.append({
+    "title": "The Power Of Now",
+    "author": "Eckhart Tolle",
+    "genere": "Sprituality, Self-Help",
+    "file_name": "the_power_of_now.pdf",
+    "relevant_start_page": 11,
+    "relevant_last_page": 0
+})
+books_data.append({
+    "title": "Rich Dad Poor Dad",
+    "author": "Robert Kiyosaki & Sharon Lechter",
+    "genere": "Personal Finance, Self-Help, Business",
+    "file_name": "rich_dad_poor_dad.pdf",
+    "relevant_start_page": 10,
+    "relevant_last_page": -3
+})
+books_data.append({
+    "title": "The Subtle Art of Not Giving A F*ck",
+    "author": "Mark Manson",
+    "genere": "Personal Development, Psychology",
+    "file_name": "the_subtle_art_of_not_giving_a_fck.pdf",
+    "relevant_start_page": 4,
+    "relevant_last_page": -9
+})
+books_data.append({
+    "title": "Meditations",
+    "author": "Marcus Aurelius",
+    "genere": "Philosophy, Personal Reflections, Classical Litrature",
+    "file_name": "meditations.pdf",
+    "relevant_start_page": 74,
+    "relevant_last_page": -28
+})
+books_data.append({
+    "title": "Man's Searching For Meaning",
+    "author": "Viktor E. Frankl",
+    "genere": "Autobiography, Psychology, Existential Philosophy",
+    "file_name": "mans_search_for_meaning.pdf",
+    "relevant_start_page": 2,
+    "relevant_last_page": -1
+})
