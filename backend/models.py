a44651e9ddbd0d7d0db4071f08fbb0b08606c967
Andualem <andualem.fereja12@gmail.com>
2025-08-23 22:57:51 +0300

    chore(backend): create pydantic models

---
+++ backend/models.py
@@ -0,0 +1,48 @@
+from pydantic import BaseModel, EmailStr
+from typing import Optional, List
+from bson import ObjectId
+
+class Token(BaseModel):
+    token: str
+
+class NewUser(BaseModel):
+    name: Optional[str] = None
+    email: EmailStr
+    password: str
+
+class UserModel(NewUser):
+    id: str
+
+    @staticmethod
+    def from_doc(doc):
+        return UserModel(
+            id=str(doc["_id"]),
+            name=doc["name"],
+            email=doc["email"],
+            password=doc["password"]
+        )
+
+class UserRegisterResponse(BaseModel):
+    message: str
+
+class UserLoginResponse(BaseModel):
+    access_token: str
+    refresh_token: str
+
+class BookModel(BaseModel):
+    id: str
+    title: str
+    author: str
+    genere: str
+
+    @staticmethod
+    def from_doc(doc):
+        return Book(
+            id=str(doc["_id"]),
+            title=doc["title"],
+            author=doc["author"],
+            genere=doc["genere"]
+        )
+
+class BooksResponseModel(BaseModel):
+    books: List[BookModel] = []
