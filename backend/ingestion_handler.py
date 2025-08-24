38d4945378d12d5ed576ff2175bb4be6568fa025
Andualem <andualem.fereja12@gmail.com>
2025-08-24 15:06:57 +0300

    feat: create ingestion process handler script

---
+++ backend/ingestion_handler.py
@@ -0,0 +1,54 @@
+from utils.pdf_handler import PdfHandler
+from utils.books_meta import books_data
+from utils.ingestion_db_init import vector_store, books_meta_collection
+from langchain_core.documents import Document
+import time
+from bson import ObjectId
+
+
+def store_book_meta(book_data: dict) -> ObjectId:
+    # Check whether book existance, if it exist return its id
+    existing_book = books_meta_collection.find_one({"title": book_data["title"]})
+    if existing_book:
+        return existing_book["_id"]
+
+    # Store a book and return its id
+    book = books_meta_collection.insert_one({
+        "title": book_data["title"],
+        "author": book_data["author"],
+        "genere": book_data["genere"]
+    })
+
+    if book is None:
+        raise Exception("can not add a book")
+
+    return book.inserted_id
+
+# Full Workflow
+if __name__ == "__main__":
+    for book_data in books_data[4:5]:
+
+        handler = PdfHandler(book_data)
+
+        chunks = handler.chunk_documents()
+
+        if chunks:
+            print("Creating embeddings for ....", book_data["title"])
+            book_id = store_book_meta(book_data)
+            print("creating documents for book id ....", book_id)
+            documents = [ Document(
+                page_content=chunk.page_content,
+                metadata={"book_id": book_id}) for chunk in chunks]
+
+            batch_size = 10
+            docs_length = len(documents)
+            for i in range(0, docs_length, batch_size):# start from batch 12 i=11
+                batch = documents[i:i + batch_size]
+                try:
+                    vector_store.add_documents(batch)
+                    print(f"Added batch {i // batch_size + 1} of {docs_length // batch_size + 1} batches")
+                    time.sleep(10)
+                except Exception as e:
+                    print(f"Error adding batch {i // batch_size + 1}: {e}")
+
+            print("Documents added successfully")
