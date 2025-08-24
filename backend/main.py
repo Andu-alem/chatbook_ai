4e0ff5ab3969b50079b015f463ceceb84e17a6e6
Andualem <andualem.fereja12@gmail.com>
2025-08-24 15:08:22 +0300

    feat: finialize route definitions and logic

--- backend/main.py
+++ backend/main.py
@@ -1,14 +1,19 @@
 from fastapi import FastAPI, Request, Depends, HTTPException
 from fastapi.security import OAuth2PasswordBearer
 from models import (
-    NewUser, UserModel, UserRegisterResponse, UserLoginResponse, BooksResponseModel
+    NewUser, UserModel, UserRegisterResponse, UserLoginResponse, BooksResponseModel, ChatQuery
 )
+from pydantic import BaseModel
+from bson import ObjectId
 from databases.base import lifespan
-from routes import projects, auth
-from index_handler.db import vector_store
+from utils.ingestion_db_init import vector_store
+from utils.jwt_helpers import decode_token
+from utils.query_helper import llm_response
+
 
 app = FastAPI(lifespan=lifespan, docs_url="/docs")
 
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
 async def get_current_user(token: str = Depends(oauth2_scheme)) -> UserModel:
     payload = decode_token(token)
     email = payload.get("email")
@@ -45,17 +50,30 @@ async def login_user(user: NewUser):
 async def refresh_token(token: str):
     return await app.users.refresh_access_token(token)
 
-@app.get("/books", response_model=BooksResponseModel)
+
+@app.get("/books", response_model=BooksResponseModel, dependencies=[Depends(get_current_user)])
 async def get_all_books():
     return await app.books.get_all_books()
 
-
 @app.post("/books/{book_id}/chat")
-async def ai_chat_interface(book_id: str, chat_query: str, current_user: UserModel = Depends(get_current_user)):
-    results = vector_store.similarity_search(chat_query, k=3)
-    
-    relevant_text = ""
-    for res in results:
-        relevant_text += res.page_content
+async def ai_chat_interface(book_id: str, chat_query: ChatQuery, current_user: UserModel = Depends(get_current_user)):
+    book_title = await get_book_by_id(id)
+    if book_title is None:
+        raise HTTPException(status_code=404, detail="No book found")
 
-    return current_user
+    # Vector Search
+    results = vector_store.similarity_search(
+        chat_query.query, 
+        k=3,
+        pre_filter={ "book_id" : { "$eq": ObjectId(book_id) } }
+    )
+
+    if not results:
+        raise HTTPException(status_code=404, detail="No relevant content found in book.")
+
+    # Merge retrived documents
+    relevant_text = " ".join([res.page_content for res in results])
+    # Call llm on relevant context
+    ai_response = llm_response(relevant_text, chat_query.query, book_title)
+
+    return ai_response
