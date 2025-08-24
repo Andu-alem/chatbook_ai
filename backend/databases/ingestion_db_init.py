bca87b884177942e1332769edb80a72629e09c6c
Andualem <andualem.fereja12@gmail.com>
2025-08-24 15:02:43 +0300

    chore: create db initialization for rag related operations

---
+++ backend/databases/ingestion_db_init.py
@@ -0,0 +1,19 @@
+from langchain_mongodb import MongoDBAtlasVectorSearch
+from pymongo import MongoClient
+from dotenv import load_dotenv
+from config import settings
+from llm_setup import embedding_model
+
+
+client = MongoClient(settings.MONGODB_URI)
+db = client[settings.DB_NAME]
+
+books_meta_collection = db["books"]
+books_chunk_collection = db["books_chunk"]
+
+vector_store = MongoDBAtlasVectorSearch(
+    collection=books_chunk_collection,
+    embedding=embedding_model,
+    index_name="default",
+    relevance_score_fn="cosine"
+)
