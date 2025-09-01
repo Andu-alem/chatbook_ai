979d9d7ba048db5c15387fc05ae23d95f91c5f30
Andualem <andualem.fereja12@gmail.com>
2025-09-01 03:10:56 +0300

    chore: create types

---
+++ frontend/app/types/types.ts
@@ -0,0 +1,13 @@
+export interface Book {
+    id: string
+    title: string
+    author: string
+    genere: string
+    description: string
+    cover_url: string
+}
+
+export interface UserType {
+    name: string
+    email: string
+}
