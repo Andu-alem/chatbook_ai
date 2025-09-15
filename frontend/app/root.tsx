5c8af3e8cda8a876998ba0d332d57a11cbf5dc2e
Andualem <andualem.fereja12@gmail.com>
2025-09-15 10:37:30 +0300

    fix: set default theme to system

--- frontend/app/root.tsx
+++ frontend/app/root.tsx
@@ -83,7 +83,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
 
 export default function App() {
   return (
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+    <ThemeProvider>
       <Outlet />
     </ThemeProvider>
   );
